77ec75f21f2c7c3191390e58dde94b96e373d7ca
Darren <qiwei9743@163.com>
2019-08-18 20:48:48 +0800

    add 705

--- src/orig/705.design-hashset.rs
+++ src/orig/705.design-hashset.rs
@@ -47,10 +47,10 @@
  * 
  * 
  */
+
 struct MyHashSet {
-
+    arr: Vec<Slist<i32>>,
 }
-
 
 /** 
  * `&self` means the method takes an immutable reference.
@@ -60,19 +60,154 @@ impl MyHashSet {
 
     /** Initialize your data structure here. */
     fn new() -> Self {
-        
+        let cnt = 1024;
+        let mut arr = Vec::with_capacity(cnt);
+        for _ in 0..cnt {
+            arr.push(Slist::new());
+        }
+        Self { arr }
+    }
+    fn calcIndex(arr_size: usize, key: &i32) -> usize {
+        *key as usize & (arr_size - 1)
+    }
+    fn add(&mut self, key: i32) {
+        let index = Self::calcIndex(self.arr.len(), &key);
+        let sl = &mut self.arr[index];
+        sl.remove(|x| *x == key);
+        sl.append(key);
     }
     
-    fn add(&self, key: i32) {
-        
-    }
-    
-    fn remove(&self, key: i32) {
-        
+    fn remove(&mut self, key: i32) {
+        let index = Self::calcIndex(self.arr.len(), &key);
+        let mut sl = &mut self.arr[index];
+        sl.remove(|x| *x == key );
     }
     
     /** Returns true if this set contains the specified element */
     fn contains(&self, key: i32) -> bool {
-        
+        let index = Self::calcIndex(self.arr.len(), &key);
+        let sl = &self.arr[index];
+        sl.iter().find(|x| **x == key).is_some()
+    }
+}
+
+struct Node<T> {
+    value: T,
+    next: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Slist<T> {
+    head: Link<T>
+}
+
+impl<i32> Default for Slist<i32> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Slist<T> {
+    fn new() -> Self {
+        Self { head: None }
+    }
+    fn peek(&self) -> Option<&T> {
+        if let Some(node) = self.head.as_ref() {
+            return Some(&node.value)
+        }
+        None
+    }
+    fn append(&mut self, elem: T) {
+        let new_node = Box::new(
+            Node{ value: elem, next: self.head.take() });
+        self.head = Some(new_node);
+    }
+    fn pop(&mut self) {
+        if let Some(node) = self.head.take() {
+            self.head = node.next;
+        }
+    }
+    fn remove<F>(&mut self, f: F) where
+        F: Fn(&T) -> bool {
+
+        let mut curr = &mut self.head;
+        loop {
+            match curr {
+                None => return,
+                Some(node) if f(&node.value) => {
+                    *curr = node.next.take();
+                    return
+                }
+                Some(node) => {
+                    curr = &mut node.next;
+                }
+            }
+        }
+    }
+    fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_ref().map(|x| &**x)}
+    }
+}
+
+struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.next.take() {
+            self.next = node.next.as_ref().map(|x| &**x);
+            return Some(&node.value);
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_slist() {
+        let mut sl = Slist::new();
+        sl.append(3);
+        assert_eq!(sl.peek(), Some(&3));
+        sl.append(2);
+        assert_eq!(sl.peek(), Some(&2));
+        sl.append(4);
+        assert_eq!(sl.peek(), Some(&4));
+        sl.pop();
+        assert_eq!(sl.peek(), Some(&2));
+        sl.pop();
+        assert_eq!(sl.peek(), Some(&3));
+        sl.pop();
+        assert_eq!(sl.peek(), None);
+        sl.append(10);
+        sl.append(12);
+        sl.append(13);
+        sl.append(14);
+        sl.remove(|x| *x == 10);
+        assert_eq!(sl.iter().find(|x| **x == 10), None);
+    }
+
+    #[test]
+    fn test_hashset() {
+        let mut hs = MyHashSet::new();
+        hs.add(10);
+        hs.add(10);
+        hs.add(11);
+        hs.add(12);
+        hs.add(13);
+        assert_eq!(hs.contains(10), true);
+        assert_eq!(hs.contains(11), true);
+        assert_eq!(hs.contains(12), true);
+        assert_eq!(hs.contains(13), true);
+        hs.remove(10);
+        assert_eq!(hs.contains(10), false);
+        assert_eq!(hs.contains(11), true);
+        assert_eq!(hs.contains(12), true);
+        assert_eq!(hs.contains(13), true);
     }
 }
