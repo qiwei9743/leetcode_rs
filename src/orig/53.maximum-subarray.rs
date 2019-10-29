60e2406b33f61ef5a4db460cfa202854a1de8f2c
Darren <qiwei9743@gmail.com>
2019-10-29 19:56:47 +0800

    update 53

--- src/orig/53.maximum-subarray.rs
+++ src/orig/53.maximum-subarray.rs
@@ -6,9 +6,9 @@
  * https://leetcode.com/problems/maximum-subarray/description/
  *
  * algorithms
- * Easy (42.05%)
- * Total Accepted:    425.4K
- * Total Submissions: 1M
+ * Easy (44.26%)
+ * Total Accepted:    656.8K
+ * Total Submissions: 1.5M
  * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
  *
  * Given an integer array nums, find the contiguous subarray (containing at
@@ -28,42 +28,17 @@
  * the divide and conquer approach, which is more subtle.
  * 
  */
-
-fn divide(nums: &Vec<i32>, start: usize, end: usize) -> i32 {
-    if start >= end {
-        nums[start]
-    } else {
-        let mid  = start + (end - start) / 2;
-        let leftmax = divide(&nums, start, mid-1);
-        let rightmax = divide(&nums, mid+1, end);
-        let mut sum = 0;
-        let mut left = 0;
-        let mut right = 0;
-        if mid - 1 >= 0 {
-            for i in mid-1..=0 {
-                sum += nums[i];
-                if left < sum {
-                    left = sum;
-                }
-            }
-        }
-        sum = 0;
-        if mid+1 <= end {
-            for i in mid+1..=end {
-                sum += nums[i];
-                if right < sum {
-                    right = sum;
-                }
-            }
-        }
-        println!("leftmax={} rightmax={} left={} rigth={} mid={}",
-                 leftmax, rightmax, left, right, mid);
-        *vec![leftmax, rightmax, nums[mid]+left+right].iter().max().unwrap()
-    }
-}
+struct Solution;
 
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        divide(&nums, 0, nums.len()-1)
+        if nums.len() == 0 {
+            return 0;
+        }
+        let mut prev = nums[0];
+        nums.iter().skip(1).map(|&x| {
+            prev = x;
+            std::cmp::max(x+prev, x)
+        }).max().unwrap_or(nums[0])
     }
 }
